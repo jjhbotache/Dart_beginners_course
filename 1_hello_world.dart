d5b3d73f647a378032eec5c2cb9a0b11b36a5643
juan <jjhuertasbotache@gmail.com>
2022-11-01 06:00:57 -0500

    day 1 learning dart

---
+++ 1_hello_world.dart
@@ -0,0 +1,18 @@
+/* 
+Here is gonna be my first hello world on dart
+ */
+
+//in here, all goes inside of a main
+//------------------------------------------------------------------------
+/* void main(List<String> args) {
+  print("hello world");
+} */
+//------------------------------------------------------------------------
+//to run it, I had to run the program in the terminal by using dart
+
+//then is asked to us to print our name
+//------------------------------------------------------------------------
+void main(List<String> args) {
+  print("Juan Jose Huertas");
+}
+//------------------------------------------------------------------------
---
+++ hello_world/bin/hello_world.dart
@@ -0,0 +1,4 @@
+//in here a created a new console proyect and then modify this file and runnig
+void main(List<String> arguments) {
+  print('Hello world');
+}
